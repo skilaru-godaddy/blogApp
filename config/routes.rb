2c646fde10e9eb2f7880b24ea7744bf43e08cbe1
skilaru-godaddy <skilaru@godaddy.com>
2025-01-20 18:48:54 +0530

    added form to create a blog post

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   root "blog_posts#index"
 
 
-  #main paths
+  # main paths
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
 end
