341004f53abde23dce3515a339beb93bb8efbf45
skilaru-godaddy <skilaru@godaddy.com>
2025-01-20 17:55:36 +0530

    added show for each blog and linked titles to the blog

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "blog_posts#index"
+
+
+  #main paths
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
 end
